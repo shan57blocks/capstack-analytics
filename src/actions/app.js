05466928eea615b2c79fb7624c064cdca3ae298c
shan <shan@57blocks.com>
2022-02-18 13:41:29 +0800

    defi portfolio page

--- src/actions/app.js
+++ src/actions/app.js
@@ -1,27 +1,8 @@
 import { createAction } from 'redux-actions'
-import api from 'src/utils/api'
 
 export const APP_SHOW_LOADING = 'APP_SHOW_LOADING'
 export const APP_CLOSE_LOADING = 'APP_CLOSE_LOADING'
 
-export const APP_OAUTH_USER = 'APP_OAUTH_USER'
-
-export const GET_AGENT_LEVELS = 'GET_AGENT_LEVELS'
-export const GET_ALL_COURSES = 'GET_ALL_COURSES'
-
-// 显示/隐藏顶层loading bar
 export const showLoadingBar = createAction(APP_SHOW_LOADING)
 
 export const closeLoadingBar = createAction(APP_CLOSE_LOADING)
-
-export const getUserInfo = createAction(APP_OAUTH_USER, () =>
-  api.get(`/user/userInfo`)
-)
-
-export const getAgentLevels = createAction(GET_AGENT_LEVELS, () =>
-  api.get(`/config/agent/level/list`)
-)
-
-export const getAllCourses = createAction(GET_ALL_COURSES, () =>
-  api.get(`/config/course/page?page=1&rows=10000`)
-)
--- src/reducers/app.js
+++ src/reducers/app.js
@@ -1,18 +1,9 @@
 import { handleActions } from 'redux-actions'
 
-import {
-  APP_SHOW_LOADING,
-  APP_CLOSE_LOADING,
-  APP_OAUTH_USER,
-  GET_AGENT_LEVELS,
-  GET_ALL_COURSES,
-} from 'src/actions/app'
+import { APP_SHOW_LOADING, APP_CLOSE_LOADING } from 'src/actions/app'
 
 const initState = {
   loading: false,
-  user: null,
-  agentLevels: [],
-  allCourses: [],
 }
 
 const app = handleActions(
@@ -27,24 +18,6 @@ const app = handleActions(
       return {
         ...state,
         loading: false,
-      }
-    },
-    [APP_OAUTH_USER]: (state, { payload }) => {
-      return {
-        ...state,
-        user: payload,
-      }
-    },
-    [GET_AGENT_LEVELS]: (state, { payload }) => {
-      return {
-        ...state,
-        agentLevels: payload,
-      }
-    },
-    [GET_ALL_COURSES]: (state, { payload }) => {
-      return {
-        ...state,
-        allCourses: payload.data,
       }
     },
   },
