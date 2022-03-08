5ca7b374e7e3ef64fa47521ab3b17f76e3c1ec25
shan <shan@57blocks.com>
2022-03-08 14:45:43 +0800

    refactor

--- src/views/positionHistory/hooks/usePositionHistory.js
+++ src/views/positionHistory/hooks/usePositionHistory.js
@@ -1,11 +1,12 @@
 import { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { useParams } from 'react-router'
+import { useHistory, useParams } from 'react-router'
 import * as appAction from 'src/actions/app'
 import { mapPosition } from 'src/utils/apy'
 import { deepClone } from 'src/utils/common'
 
 export const usePositionHistory = () => {
+  const history = useHistory()
   const dispatch = useDispatch()
   const { id: positionId } = useParams()
   const { protocolPositions, histories } = useSelector((state) => state.app)
@@ -27,6 +28,11 @@ export const usePositionHistory = () => {
       clonedPosition = deepClone(positions[positionIndex])
     }
   })
+
+  if (!clonedPosition) {
+    history.push('/')
+    return null
+  }
 
   clonedPosition.histories = clonedPosition.histories.concat(histories)
   return mapPosition(clonedPosition)
