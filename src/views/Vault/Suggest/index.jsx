8a54d0b73395542e3cf47e82c4a835e13c2a7a31
shan <shan@57blocks.com>
2022-04-19 15:39:45 +0800

    suggestion

--- src/views/Vault/Suggest/index.jsx
+++ src/views/Vault/Suggest/index.jsx
@@ -1,12 +1,28 @@
 import './index.less'
 
-import { Form, Input, Modal, Space, Table } from 'antd'
-import React, { useState } from 'react'
+import { Form, Input, InputNumber, Modal, Space, Table } from 'antd'
+import React, { useEffect, useState } from 'react'
 import CapSkeleton from 'src/components/CapSkeleton'
 import { toPercentage } from 'src/utils/common'
 
 const Suggest = ({ vault }) => {
+  const [strategies, setStrategies] = useState()
   const [isModalVisible, setIsModalVisible] = useState(false)
+  const unallocated = vault ? Number(vault.unallocated) : 0
+
+  useEffect(() => {
+    setStrategies(renforceStrategies(vault, vault?.strategies))
+  }, [vault])
+
+  const changeLeverage = (leverage, strategyId) => {
+    const newStrategies = strategies.map((item) => {
+      if (item.id === strategyId) {
+        item.leverage = leverage
+      }
+      return item
+    })
+    setStrategies(renforceStrategies(vault, newStrategies))
+  }
 
   const showModal = () => {
     setIsModalVisible(true)
@@ -32,12 +48,12 @@ const Suggest = ({ vault }) => {
     <div className="vault-suggest">
       <div className="vault-suggest-change">
         <div>Uninvested:</div>
-        <div>{vault.unallocated}</div>
+        <div>{unallocated}</div>
       </div>
       <Table
         rowKey="id"
-        columns={getColumns(vault, showModal)}
-        dataSource={vault.strategies}
+        columns={getColumns(vault, changeLeverage, showModal)}
+        dataSource={strategies}
         bordered
       />
       <Modal
@@ -74,7 +90,7 @@ const Suggest = ({ vault }) => {
 
 export default Suggest
 
-const getColumns = (vault, showModal) => [
+const getColumns = (vault, changeLeverage, showModal) => [
   {
     title: 'Name',
     dataIndex: 'name',
@@ -109,9 +125,35 @@ const getColumns = (vault, showModal) => [
     },
   },
   {
+    title: 'Leverage',
+    dataIndex: 'leverage',
+    key: 'leverage',
+    width: 160,
+    render: (leverage, strategy) => {
+      return (
+        <InputNumber
+          min={1}
+          width={80}
+          step={0.1}
+          value={leverage}
+          onChange={(value) => changeLeverage(value, strategy.id)}
+        ></InputNumber>
+      )
+    },
+  },
+  {
     title: 'Suggestion',
-    dataIndex: 'action',
-    key: 'action',
+    dataIndex: 'suggestions',
+    key: 'suggestions',
+    render: (suggestions) => {
+      return (
+        <div>
+          {suggestions.map((item) => (
+            <div key={item}>{item}</div>
+          ))}
+        </div>
+      )
+    },
   },
   {
     title: 'Action',
@@ -123,3 +165,38 @@ const getColumns = (vault, showModal) => [
     ),
   },
 ]
+
+const renforceStrategies = (vault, strategies) => {
+  if (!vault) {
+    return []
+  }
+  return strategies.map((strategy) => {
+    strategy.principal = Number(vault.unallocated) * strategy.percentage
+    strategy.suggestions = []
+    strategy.leverage = strategy.leverage ?? 3
+    if (strategy.positions.length > 1) {
+      const principal0 =
+        (strategy.principal * strategy.leverage) / (2 * strategy.leverage - 2)
+      const principal1 =
+        (strategy.principal * (strategy.leverage - 2)) /
+        (2 * strategy.leverage - 2)
+      strategy.suggestions.push(
+        `Invest ${principal0} ${vault.name} in first position`
+      )
+      strategy.suggestions.push(
+        `Invest ${principal1} ${vault.name} in second position`
+      )
+    } else {
+      const [{ tokens }] = strategy.positions
+      const borrowRate0 = (strategy.leverage / 2 - 1).toFixed(2)
+      const borrowRate1 = (strategy.leverage / 2).toFixed(2)
+      strategy.suggestions.push(
+        `Borrow ${borrowRate0}X ${vault.name} for ${tokens[0].symbol}`
+      )
+      strategy.suggestions.push(
+        `Borrow ${borrowRate1}X ${vault.name} for ${tokens[1].symbol}`
+      )
+    }
+    return strategy
+  })
+}
